412a2d4f011f0509d7ef971531603c852f3ed5cc
VijayalakshmiSenthil <vijayalakshmisenthil8133@gmail.com>
2024-10-09 13:09:52 +0530

    changes in feedback session

--- components/feedback.js
+++ components/feedback.js
@@ -1,28 +1,28 @@
 export default function Feedback() {
   return (
     <>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <video autoplay width="300" height="340" muted loop>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <video autoPlay width="300" height="340" muted loop>
               <source src="/images/feedback.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
-          <div class="col-md-8 align-items-center d-flex justify-content-center center">
-            <p class="text-align">
+          <div className="col-md-8 align-items-center d-flex justify-content-center center">
+            <p className="text-align">
               <b>Contact Us</b>
-              <br></br>
+              <br />
               1.4 million SBA loans are currently past due, delinquent, or in
-              liquidation. Don't risk falling into financial trouble with your
-              EIDL loan. If you're facing difficulties with repayment, contact
+              liquidation. Don&apos;t risk falling into financial trouble with your
+              EIDL loan. If you&apos;re facing difficulties with repayment, contact
               us today for personalized assistance. We can help you explore
-              options to manage your debt and avoid default. Don't wait—reach
+              options to manage your debt and avoid default. Don&apos;t wait—reach
               out now to secure your financial future and keep your business on
               track!
             </p>
-            <div class="parent-container">
-              <img src="/images/design.png" alt="design" height="100px"></img>
+            <div className="parent-container">
+              <img src="/images/design.png" alt="design" height="100px" />
             </div>
           </div>
         </div>
